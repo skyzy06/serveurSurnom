df7ae01f62eace36269e64abba885c15d10766ca
Thomas <Thomas@10.212.117.203>
2014-11-05 11:43:39 +0100

    for boys

--- ServeurSurnom/src/protocole/AddCommand.java
+++ ServeurSurnom/src/protocole/AddCommand.java
@@ -6,33 +6,33 @@ import java.util.List;
 
 public class AddCommand extends Command {
 
-	// => parameter for the request to the server
-	String name = "";
-	List<String> nicknames = new LinkedList<String>();
-	
-	// <= eventual error message in the response of the server
-	String errorMsg = "";
+    // => parameter for the request to the server
+    String name = "";
+    List<String> nicknames = new LinkedList<String>();
 
-	public AddCommand(boolean succeed, Calendar date, String name,
+    // <= eventual error message in the response of the server
+    String errorMsg = "";
+
+    public AddCommand(boolean succeed, Calendar date, String name,
             List<String> nicknames) {
-	    super(succeed, date);
-	    this.name = name;
-	    this.nicknames = nicknames;
+        super(succeed, date);
+        this.name = name;
+        this.nicknames = nicknames;
     }
 
-	public String getErrorMsg() {
-    	return errorMsg;
+    public String getErrorMsg() {
+        return errorMsg;
     }
 
-	public void setErrorMsg(String errorMsg) {
-    	this.errorMsg = errorMsg;
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
     }
 
-	public String getName() {
-    	return name;
+    public String getName() {
+        return name;
     }
 
-	public List<String> getNicknames() {
-    	return nicknames;
+    public List<String> getNicknames() {
+        return nicknames;
     }
 }
