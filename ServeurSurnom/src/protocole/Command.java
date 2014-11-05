5014530bb2c14399c2b0612155b9bcc76bd39ba1
thomas <>
2014-11-05 12:43:23 +0100

    test changement de pc

--- ServeurSurnom/src/protocole/Command.java
+++ ServeurSurnom/src/protocole/Command.java
@@ -3,27 +3,27 @@ package protocole;
 import java.util.Calendar;
 
 public class Command {
-	    // => parameter for the request to the server
-		Calendar date = Calendar.getInstance();
-		
-		// <= response to the server
-		boolean isSucceed = false;
-		
 
-		public Command(boolean succeed, Calendar date) {
-	        this.isSucceed = succeed;
-	        this.date = date;
-        }
+    // => parameter for the request to the server
+    Calendar date = Calendar.getInstance();
 
-		public boolean isSucceed() {
-        	return isSucceed;
-        }
+    // <= response to the server
+    boolean isSucceed = false;
 
-		public void setSucceed(boolean isSucceed) {
-        	this.isSucceed = isSucceed;
-        }
+    public Command(boolean succeed, Calendar date) {
+        this.isSucceed = succeed;
+        this.date = date;
+    }
 
-		public Calendar getDate() {
-        	return date;
-        }
+    public boolean isSucceed() {
+        return isSucceed;
+    }
+
+    public void setSucceed(boolean isSucceed) {
+        this.isSucceed = isSucceed;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
 }
