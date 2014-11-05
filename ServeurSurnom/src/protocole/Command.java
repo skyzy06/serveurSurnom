e6c18ed453459e8d95b1d4f8ccfd9aff8a89534a
Anaïs MARONGIU <ma102729@etu.unice.fr>
2014-11-05 11:21:39 +0100

    Protocole V1

---
+++ ServeurSurnom/src/protocole/Command.java
@@ -0,0 +1,29 @@
+package protocole;
+
+import java.util.Calendar;
+
+public class Command {
+	    // => parameter for the request to the server
+		Calendar date = Calendar.getInstance();
+		
+		// <= response to the server
+		boolean isSucceed = false;
+		
+
+		public Command(boolean succeed, Calendar date) {
+	        this.isSucceed = succeed;
+	        this.date = date;
+        }
+
+		public boolean isSucceed() {
+        	return isSucceed;
+        }
+
+		public void setSucceed(boolean isSucceed) {
+        	this.isSucceed = isSucceed;
+        }
+
+		public Calendar getDate() {
+        	return date;
+        }
+}
