15db3dc4e971a2b168b3e1fecc6e1c6322837e8b
Administrateur <Administrateur@10.212.99.171>
2014-11-12 11:35:09 +0100

    client add

--- ServeurSurnom/src/client/Client.java
+++ ServeurSurnom/src/client/Client.java
@@ -5,18 +5,11 @@
  */
 package client;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
-import java.net.BindException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import protocole.*;
 
 /**
  *
@@ -57,10 +50,10 @@ public class Client {
     }
 
     /**
-     * Méthode permettant d'interprété les commandes du client
+     * Methode permettant d'interprete les commandes du client
      */
-    public boolean commandPrompt() {
-        System.out.println("Choississez la requète à exécuter ou \"quit\" pour sortir");
+    public boolean commandPrompt() throws IOException {
+        System.out.println("Choississez la requete a executer ou \"quit\" pour sortir");
         System.out.println("ADD : pour ajouter un nom et un surnom");
         System.out.println("LIST : pour lister l'ensemble des surnoms");
 
@@ -73,21 +66,48 @@ public class Client {
             choix = sc.next();
             requete = commandSelection(choix);
         } while (correct == false || choix == "quit");
-
+        ObjectInputStream ois = new ObjectInputStream(socketClient.getInputStream());
+        try {
+            while (true) {
+                AddCommand cmd = (AddCommand) ois.readObject();
+                if (cmd != null) {
+                    System.out.println("Result : " + cmd.isSucceed());
+                    if (!cmd.isSucceed()) {
+                        System.out.println("Result : " + cmd.isSucceed() + cmd.getErrorMsgs().toString());
+                    }
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        out.writeObject(new ExitCommand());
+        in.close();
+        out.close();
+        socketClient.close();
         return correct;
     }
 
     /**
-     * Méthode permettant d'exécuter la commande demandée
+     * Methode permettant d'executer la commande demandee
      *
-     * @param choix la commande à exécuter
-     * @return la réponse de la commande
+     * @param choix la commande a executer
+     * @return la reponse de la commande
      */
-    private String commandSelection(String choix) {
+    private String commandSelection(String choix) throws IOException {
         switch (choix) {
-            //TODO remplacer par les appels de méthodes
+            //TODO remplacer par les appels de mÃ©thodes
             case "ADD":
-                return "ADD";
+                String nom;
+                List<String> surnom = new LinkedList<String>();
+                System.out.print("A quel nom voulez-vous ajouter un surnom? : ");
+                nom = sc.next();
+                do {
+                    System.out.print("Quel surnom voulez-vous lui attribuer? : ");
+                    surnom.add(sc.next());
+                    System.out.print("Voulez-vous en ajouter un autre? : ");
+                } while (!sc.next().equals("no"));
+                out.writeObject(new AddCommand(nom, surnom));
             case "LIST":
                 return "LIST";
         }
@@ -101,7 +121,5 @@ public class Client {
         } catch (IOException e) {
             return false;
         }
-
     }
-
 }
