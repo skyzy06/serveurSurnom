e1d9245575369e0b86f5f455f5e5b46898402b03
Audric Chabert <achabert@polytech.unice.fr>
2014-11-12 11:02:16 +0100

    - ObjectOutputStream use - removal of ServerLauncher & ClientLauncher

--- ServeurSurnom/src/client/Client.java
+++ ServeurSurnom/src/client/Client.java
@@ -9,7 +9,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
+import java.net.BindException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -20,39 +24,60 @@ import java.util.Scanner;
  */
 public class Client {
 
+	String hostName;
+    int portNumber;
+
     private Socket socketClient;
-    private BufferedWriter out;
-    private BufferedReader in;
+    
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    
     private Scanner sc = new Scanner(System.in);
+    
 
     public Client(String hostName, int portNumber) {
-        try {
-            socketClient = new Socket(hostName, portNumber);
-            out = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
-            in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
-            System.out.println("connecté au serveur");
-        } catch (UnknownHostException e) {
-            System.err.println("Impossible de se connecter à " + hostName);
-        } catch (IOException e) {
-        }
+        this.hostName = hostName;
+        this.portNumber = portNumber;
     }
 
+    public void connect() throws UnknownHostException, IOException{
+            socketClient = new Socket();
+            socketClient.setReuseAddress(true);
+            try{
+            socketClient.bind(new InetSocketAddress(hostName,portNumber));    
+            }catch(BindException be){
+            	System.out.println("erreur de bind");
+            	throw new IOException();
+            }
+            out = new ObjectOutputStream(socketClient.getOutputStream());
+            in = new ObjectInputStream(socketClient.getInputStream());
+    }
+    
+    public void disconnect() throws IOException{
+    	socketClient.close();
+    }
+    
     /**
      * Méthode permettant d'interprété les commandes du client
      */
-    public void commandReader() {
+    public boolean commandPrompt() {
         System.out.println("Choississez la requète à exécuter ou \"quit\" pour sortir");
         System.out.println("ADD : pour ajouter un nom et un surnom");
         System.out.println("LIST : pour lister l'ensemble des surnoms");
 
         String choix;
         String requete = "";
+        boolean correct = false;
+        
         do {
             System.out.print("Votre choix : ");
             choix = sc.next();
             requete = commandSelection(choix);
-        } while (!choix.equals("quit"));
+        } while (correct==false || choix=="quit");
+        
+        return correct;
     }
+    
 
     /**
      * Méthode permettant d'exécuter la commande demandée
@@ -70,4 +95,15 @@ public class Client {
         }
         return "Default";
     }
+    
+    public boolean sendTest(){
+    	try {
+			out.writeUTF("test");
+			return true;
+    	} catch (IOException e) {
+			return false;
+		}
+ 
+    }
+    
 }
--- ServeurSurnom/src/protocole/ExitCommand.java
+++ ServeurSurnom/src/protocole/ExitCommand.java
@@ -1,6 +1,9 @@
 package protocole;
 
-
+/*
+ * Exit command
+ * fin de la communication client-serveur
+ */
 public class ExitCommand extends Command {
 	private static final long serialVersionUID = 1L;
 
